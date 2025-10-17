b984fa745f32a1a7c569815a921f6817dc82e6c1
idkwaat <pck06092005@gmail.com>
2025-10-18 02:47:28 +0700

    abcde

--- ProjectApi/Dtos/OrderItemRequest.cs
+++ ProjectApi/Dtos/OrderItemRequest.cs
@@ -3,7 +3,10 @@
     public class OrderItemRequest
     {
         public int ProductId { get; set; }
+        public int? VariantId { get; set; }
+        public string? VariantName { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
+
 }
