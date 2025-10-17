708887c723b3544cd49a573318cf5074adac2936
idkwaat <pck06092005@gmail.com>
2025-10-18 01:08:32 +0700

    pls

--- ProjectApi/obj/Release/net8.0/ProjectApi.AssemblyInfo.cs
+++ ProjectApi/obj/Release/net8.0/ProjectApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64f7e96bdfa5cec86bf2c572e73a3b2722cbcf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectApi")]
